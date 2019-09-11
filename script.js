ed04ccc6388bf9f40b020663094143d75a5c64b7
sreedharanp <sreedharan.p@gmail.com>
2019-09-11 15:18:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,109 @@
+I clearly asked for the code in githublink. That is mentioned in the mail & I even mentioned it over call.
+
+Please share the git hub link.
+
+ 
+
+Best Regards,
+
+Ramen Dey
+
+Cell +91 9987797627
+
+cid:02bf8637-f64b-41ce-bdaf-3a5339a21e06
+
+ 
+
+From: Sreedharan Parayil <sreedharan_p@yahoo.com>
+Sent: Wednesday, September 11, 2019 2:10 AM
+To: Kumar Dey, Ramen <ramen.kumar.dey@accenture.com>
+Subject: [External] Re: Coding Problem Statement - FE Dev - 10 Sept
+
+ 
+
+This message is from an EXTERNAL SENDER - be CAUTIOUS, particularly with links and attachments.
+
+ 
+
+Pls find the solutions
+
+ 
+
+1
+
+ 
+
+let person = {
+
+  firstName: "John",
+
+  lastName : "Doe", 
+
+  fullName : function() {
+
+    return this.firstName + " " + this.lastName;
+
+  }
+
+};
+
+//////////////
+ 
+
+var name = "Hello world";
+
+var count = {};
+
+ 
+
+for (var i = 0, n = name.length; i < n; i++) {
+
+    var char = name[i];
+
+    count[char] = (count[char] || 0) + 1;
+
+}
+
+ 
+
+console.log(count);
+
+ ////////////
+ 
+
+var map = { 'a' : '5', 'b' : '', 'c': '', 'd': 10 };
+
+var keys = [];
+
+for (var key in map) {
+
+    keys.push(key);
+
+}
+
+ 
+
+document.body.textContent = keys;
+
+ 
+////////////
+ 
+
+function reverseString(str) {   
+
+    var reverseArray = splitString.reverse();  
+
+}
+
+ 
+
+reverseString("hello");
+
+ ////////
+ 
+
+const numbers = [2,3,4,4,2,3,3,4,4,5,5,6,6,7,5,32];
+
+ 
+
+console.log([...new Set(numbers)]);
